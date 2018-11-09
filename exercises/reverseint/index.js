e96f756fa2ff9b2b82acdbe231f7331587a8e97e
dahby <dstoll243@gmail.com>
2018-11-09 14:00:18 -0800

    completed reverseint

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,17 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+  const stringedN = n.stringify();
+  let reversed = '';
+  for (let i = stringedN.length - 1; i >= 0; i--) {
+    if (stringedN[i] === '-') {
+      reversed = stringedN[i] + reversed;
+    } else {
+      reversed += stringedN;
+    }
+  }
+  return parseInt(stringedN);
+}
 
 module.exports = reverseInt;
