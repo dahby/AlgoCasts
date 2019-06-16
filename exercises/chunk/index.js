d7e9ed32794e172dccde616d0fdcb6ad5b6be1bc
dahby <dstoll243@gmail.com>
2019-06-16 10:55:33 -0700

    finished even more on chunk!

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,51 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+// function chunk(array, size) {
+//     const chunked = [];
+//     let subArray = [];
+//     for (int of array) {
+//         subArray.push(int);
+//         if (subArray.length === size) {
+//             chunked.push(subArray);
+//             subArray = [];
+//         }
+//     }
+//     if (subArray.length > 0) {
+//         chunked.push(subArray);
+//     }
+//     return chunked;
+// }
+
+// their solution
+
+// function chunk(array, size) {
+//     const chunked = [];
+
+//     for (let element of array) {
+//         const last = chunked[chunked.length];
+
+//         if (!last || last.length === size) {
+//             chunked.push([element]);
+//         } else {
+//             last.push(element);
+//         }
+//     }
+//     return chunked;
+// }
+
+// SOLUTION W/ SLICE
+
+function chunk(array, size) {
+    const chunked = [];
+    let index = 0;
+    while (index < array.length) {
+        chunked.push(array.slice(index, size + index));
+        index += size;
+    }
+    return chunked;
+}
+ 
+chunk([1, 2, 3, 4, 5], 2)
 
 module.exports = chunk;
